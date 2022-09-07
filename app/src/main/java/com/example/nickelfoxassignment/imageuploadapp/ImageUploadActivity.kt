a860fcb3c71c36ac46d4a7eaef2e75ad2b4a8247
shagun-nickelfox <shagun.bansal@nickelfox.com>
2022-09-07 22:04:17 +0530

    refactor(takephoto) : take photo with camera using androidx result api

--- app/src/main/java/com/example/nickelfoxassignment/imageuploadapp/ImageUploadActivity.kt
+++ app/src/main/java/com/example/nickelfoxassignment/imageuploadapp/ImageUploadActivity.kt
@@ -1,28 +1,41 @@
 package com.example.nickelfoxassignment.imageuploadapp
 
 import android.Manifest
+import android.content.ContentValues
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.ParcelFileDescriptor
+import android.provider.MediaStore
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import androidx.core.view.isVisible
 import androidx.lifecycle.lifecycleScope
+import com.example.nickelfoxassignment.BuildConfig
 import com.example.nickelfoxassignment.R
 import com.example.nickelfoxassignment.databinding.ActivityImageUploadBinding
 import com.example.nickelfoxassignment.imageuploadapp.viewmodel.ImageUploadViewModel
 import com.example.nickelfoxassignment.shortToast
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
+import java.io.File
+import java.io.FileDescriptor
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 @AndroidEntryPoint
 class ImageUploadActivity : AppCompatActivity() {
     private lateinit var binding: ActivityImageUploadBinding
     private val imageViewModel by viewModels<ImageUploadViewModel>()
     private var imageUri: Uri? = null
+    private var latestTmpUri: Uri? = null
 
     private val selectImage =
         registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
@@ -32,12 +45,32 @@ class ImageUploadActivity : AppCompatActivity() {
             }
         }
 
-    private val requestPermissions =
+    private val takeImageResult =
+        registerForActivityResult(ActivityResultContracts.TakePicture()) { isSuccess ->
+            if (isSuccess) {
+                latestTmpUri?.let { uri ->
+                    binding.ivSelectedImage.setImageURI(uri)
+                    imageUri = uri
+                    galleryAddPic()
+                }
+            }
+        }
+
+    private val requestReadPermissions =
         registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
             if (isGranted) {
                 chooseImageGallery()
             } else {
                 this.shortToast(resources.getString(R.string.permission_denied))
+            }
+        }
+
+    private val requestWritePermissions =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
+            if (it.any { permission -> !permission.value }) {
+                this.shortToast(resources.getString(R.string.permission_denied))
+            } else {
+                takeImage()
             }
         }
 
@@ -63,13 +96,24 @@ class ImageUploadActivity : AppCompatActivity() {
                 )
             )
             btnSelectImage.setOnClickListener {
+                showImageSelectOptions(true)
+            }
+            btnTakeImageButton.setOnClickListener {
+                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+                    takeWritePermissions()
+                } else {
+                    takeImage()
+                }
+            }
+            btnSelectImageButton.setOnClickListener {
                 if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.Q) {
-                    takePermissions()
+                    takeReadPermissions()
                 } else {
                     chooseImageGallery()
                 }
             }
             btnUploadImage.setOnClickListener {
+                showImageSelectOptions(false)
                 if (imageUri != null) {
                     viewVisibility(btnVisibility = false, progressBarVisibility = true)
                     imageViewModel.selectedImageUri(imageUri!!)
@@ -95,16 +139,95 @@ class ImageUploadActivity : AppCompatActivity() {
         }
     }
 
-    private fun takePermissions() {
+    private fun showImageSelectOptions(visible: Boolean) {
+        binding.btnSelectImageButton.isVisible = visible
+        binding.btnTakeImageButton.isVisible = visible
+    }
+
+    private fun takeReadPermissions() {
         if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
-            requestPermissions.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+            requestReadPermissions.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
         } else {
             chooseImageGallery()
         }
     }
 
+    private fun takeWritePermissions() {
+        if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED || checkSelfPermission(
+                Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_DENIED
+        ) {
+            requestWritePermissions.launch(
+                arrayOf(
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.CAMERA
+                )
+            )
+        } else {
+            takeImage()
+        }
+    }
+
     private fun chooseImageGallery() {
         selectImage.launch("image/*")
+    }
+
+    private fun takeImage() {
+        lifecycleScope.launchWhenStarted {
+            getTmpFileUri().let { uri ->
+                latestTmpUri = uri
+                takeImageResult.launch(uri)
+            }
+        }
+    }
+
+    private fun getTmpFileUri(): Uri {
+        val tmpFile = File.createTempFile(createImageName(), ".jpg", cacheDir).apply {
+            createNewFile()
+        }
+        return FileProvider.getUriForFile(
+            applicationContext,
+            "${BuildConfig.APPLICATION_ID}.provider",
+            tmpFile
+        )
+    }
+
+    private fun createImageName(): String {
+        val timeStamp = SimpleDateFormat.getDateTimeInstance().format(Date())
+        return "JPEG_${timeStamp}_"
+    }
+
+    private fun getBitmapFromUri(uri: Uri?): Bitmap {
+        val parcelFileDescriptor: ParcelFileDescriptor? =
+            contentResolver.openFileDescriptor(uri!!, "r")
+        val fileDescriptor: FileDescriptor? = parcelFileDescriptor?.fileDescriptor
+        val image: Bitmap = BitmapFactory.decodeFileDescriptor(fileDescriptor)
+        parcelFileDescriptor?.close()
+        return image
+    }
+
+    private fun galleryAddPic() {
+        val imageCollection = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY)
+        } else {
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        }
+        val bitMap = getBitmapFromUri(latestTmpUri)
+        val contentValues = ContentValues().apply {
+            put(MediaStore.Images.Media.DISPLAY_NAME, "${createImageName()}.jpg")
+            put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
+        }
+        contentResolver.insert(imageCollection, contentValues)
+        try {
+            contentResolver.insert(imageCollection, contentValues)?.also { uri ->
+                contentResolver.openOutputStream(uri).use { outputStream ->
+                    if (!bitMap.compress(Bitmap.CompressFormat.JPEG, 95, outputStream))
+                        throw IOException("Couldn't save bitmap")
+                }
+            } ?: throw IOException("Couldn't create MediaStore entry.")
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
     }
 
     private fun viewVisibility(btnVisibility: Boolean, progressBarVisibility: Boolean) {
