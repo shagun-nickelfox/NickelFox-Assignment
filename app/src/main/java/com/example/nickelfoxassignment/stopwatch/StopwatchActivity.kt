37013003cf35d702c99fe2d0079394723b1e3913
shagun-nickelfox <shagun.bansal@nickelfox.com>
2022-09-13 12:46:17 +0530

    refactor(createUI):create UI design for stopwatch

---
+++ app/src/main/java/com/example/nickelfoxassignment/stopwatch/StopwatchActivity.kt
@@ -0,0 +1,12 @@
+package com.example.nickelfoxassignment.stopwatch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.nickelfoxassignment.R
+
+class StopwatchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_stopwatch)
+    }
+}
